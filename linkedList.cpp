1a2d6c87611d8248a0e324d790c26029eba0b3cc
Lalit Thakre <lalit03thakre@gmail.com>
2022-10-15 15:22:33 +0530

    new

---
+++ linkedList.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+void reverse(list<int>& li )
+{
+    int data;
+    for(int i=;i!=NULL;i++)
+    {
+cout<<
+    }
+} 
+int main()
+{
+    list<int>li;
+    li.push_back(1);
+    li.push_back(2);
+    li.push_back(3);
+    li.push_back(4);
+    li.push_back(5);
+    li.push_back(6);
+    li.push_back(7);
+    li.push_back(8);
+    reverse(li);
+}
