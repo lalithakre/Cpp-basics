15470f0614b017ee45ce37fc996a22e394cbc58e
Lalit Thakre <lalit03thakre@gmail.com>
2023-03-30 09:23:52 +0530

    commit

--- const.cpp
+++ const.cpp
@@ -1,9 +1,9 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    const int a=10000;
+    const int a = 10000;
     // a+=10;
-    cout<<a;
+    cout << a;
     return 0;
 }
